4319ccd352b7a95a2e539ddd95475d294193c2b5
Ramiro Algozino <algozino@gmail.com>
2011-04-12 00:12:13 -0300

    Updating to latest launchpad version

--- qtqr.py
+++ qtqr.py
@@ -22,18 +22,54 @@ class MainWindow(QtGui.QMainWindow):
         self.setWindowIcon(QtGui.QIcon(u'icon.png'))
         self.w = QtGui.QWidget()
         self.setCentralWidget(self.w)
+
+        #Tabs
+        self.tabs = QtGui.QTabWidget()
+        self.textTab = QtGui.QWidget()
+        self.urlTab = QtGui.QWidget()
+        self.emailTab = QtGui.QWidget()
+        self.smsTab = QtGui.QWidget()
+        self.telTab = QtGui.QWidget()
+        self.tabs.addTab(self.textTab, u"&Text")
+        self.tabs.addTab(self.urlTab, u"&URL")
+        self.tabs.addTab(self.emailTab, u"&Email")
+        self.tabs.addTab(self.smsTab, u"S&MS")
+        self.tabs.addTab(self.telTab, u"&Telephone")
+        
+        self.l1 = QtGui.QLabel(u'Text to be encoded:')
+        self.textEdit = QtGui.QPlainTextEdit()
+
+        self.urlLabel =  QtGui.QLabel(u'URL to be encoded:')
+        self.urlEdit = QtGui.QLineEdit(u'http://')
+        
+        self.emailLabel = QtGui.QLabel(u'E-Mail address:')
+        self.emailEdit = QtGui.QLineEdit(u"@.com")
+        self.emailSubLabel = QtGui.QLabel(u'Subject:')
+        self.emailSubjectEdit = QtGui.QLineEdit()
+        self.emailBodyLabel = QtGui.QLabel(u'Message Body:')
+        self.emailBodyEdit = QtGui.QLineEdit()
+        self.telephoneLabel = QtGui.QLabel(u'Telephone Number:')
+        self.telephoneEdit = QtGui.QLineEdit()
+        self.smsNumberLabel = QtGui.QLabel(u'Telephone Number:')
+        self.smsNumberEdit = QtGui.QLineEdit() 
+        self.smsBodyLabel = QtGui.QLabel(u'Message:')
+        self.smsBodyEdit = QtGui.QLineEdit()
+
         self.optionsGroup = QtGui.QGroupBox(u'Parameters:')
-        self.lineEdit = QtGui.QLineEdit()
-        self.l1 = QtGui.QLabel(u'Insert &text to be encoded:')
-        self.qrcode = QtGui.QLabel(u'\n\nStart typing to create QRcode.\n\n')
-        self.qrcode.setAlignment(QtCore.Qt.AlignHCenter)
+
         self.l2 = QtGui.QLabel(u'&Pixel Size:')
         self.pixelSize = QtGui.QSpinBox()
+
         self.l3 = QtGui.QLabel(u'&EC Level:')
         self.ecLevel = QtGui.QComboBox() #LMQH
         self.ecLevel.addItems((u'Lowest',u'Medium',u'Q',u'Highest'))
+
         self.l4 = QtGui.QLabel(u'&Margin Size:')
         self.marginSize = QtGui.QSpinBox()
+
+        self.qrcode = QtGui.QLabel(u'\n\nStart typing to create QRcode.\n\n')
+        self.qrcode.setAlignment(QtCore.Qt.AlignHCenter)
+
         self.saveButton = QtGui.QPushButton(QtGui.QIcon.fromTheme(u'document-save'), u'&Save QRCode')
         self.exitButton = QtGui.QPushButton(QtGui.QIcon.fromTheme(u'application-exit'),u'E&xit')
         self.decodeButton = QtGui.QPushButton(QtGui.QIcon.fromTheme(u'preview-file'),u'&Decode')
@@ -42,38 +78,82 @@ class MainWindow(QtGui.QMainWindow):
         self.decodeFileAction = self.decodeMenu.addAction(QtGui.QIcon.fromTheme(u'document-open'), u'Decode from File')
         self.decodeWebcamAction = self.decodeMenu.addAction(QtGui.QIcon.fromTheme(u'image-png'), u'Decode from WebCam')
         self.decodeButton.setMenu(self.decodeMenu)
-        
+
+        self.textEdit.setMaximumHeight(self.textEdit.height()/3.5)        
         self.qrcode.setFrameShape(QtGui.QFrame.StyledPanel)
         self.saveButton.setEnabled(False)
         self.pixelSize.setValue(3)
         self.pixelSize.setMinimum(1)
         self.marginSize.setValue(4)
-        self.l1.setBuddy(self.lineEdit)
+        self.l1.setBuddy(self.textEdit)
         self.l2.setBuddy(self.pixelSize)
         self.l3.setBuddy(self.ecLevel)
         self.l4.setBuddy(self.marginSize)
         self.ecLevel.setToolTip(u'Error Correction Level')
+        self.exitButton.setShortcut(u"Ctrl+Q")
+        self.saveButton.setShortcut(u"Ctrl+S")
 
         self.buttons = QtGui.QHBoxLayout()
         self.buttons.addWidget(self.saveButton)
         self.buttons.addWidget(self.decodeButton)
         
+        #Text Tab
         self.codeControls = QtGui.QVBoxLayout()
         self.codeControls.addWidget(self.l1)
-        self.codeControls.addWidget(self.lineEdit)
+        self.codeControls.addWidget(self.textEdit)
+        self.codeControls.addStretch()
+        self.textTab.setLayout(self.codeControls)
         
+        #URL Tab
+        self.urlTabLayout = QtGui.QVBoxLayout()
+        self.urlTabLayout.addWidget(self.urlLabel)
+        self.urlTabLayout.addWidget(self.urlEdit)
+        self.urlTabLayout.addStretch()
+        self.urlTab.setLayout(self.urlTabLayout)
+        
+        #Email Tab
+        self.emailTabLayout = QtGui.QVBoxLayout()
+        self.emailTabLayout.addWidget(self.emailLabel)
+        self.emailTabLayout.addWidget(self.emailEdit)
+        self.emailTabLayout.addWidget(self.emailSubLabel)
+        self.emailTabLayout.addWidget(self.emailSubjectEdit)
+        self.emailTabLayout.addWidget(self.emailBodyLabel)
+        self.emailTabLayout.addWidget(self.emailBodyEdit)
+        self.emailTabLayout.addStretch()
+        self.emailTab.setLayout(self.emailTabLayout)
+        
+        #SMS Tab
+        self.smsTabLayout = QtGui.QVBoxLayout()
+        self.smsTabLayout.addWidget(self.smsNumberLabel)
+        self.smsTabLayout.addWidget(self.smsNumberEdit)
+        self.smsTabLayout.addWidget(self.smsBodyLabel)
+        self.smsTabLayout.addWidget(self.smsBodyEdit)
+        self.smsTabLayout.addStretch()
+        self.smsTab.setLayout(self.smsTabLayout)
+        
+        #Telephone Tab
+        self.telTabLayout = QtGui.QVBoxLayout()
+        self.telTabLayout.addWidget(self.telephoneLabel)
+        self.telTabLayout.addWidget(self.telephoneEdit)
+        self.telTabLayout.addStretch()
+        self.telTab.setLayout(self.telTabLayout)
+        
+        #Pixel Size Controls
         self.pixControls = QtGui.QVBoxLayout()
         self.pixControls.addWidget(self.l2)
         self.pixControls.addWidget(self.pixelSize)
         
+        #Error Correction Level Controls
         self.levelControls = QtGui.QVBoxLayout()
         self.levelControls.addWidget(self.l3)
         self.levelControls.addWidget(self.ecLevel)
         
+        #Margin Size Controls
         self.marginControls = QtGui.QVBoxLayout()
         self.marginControls.addWidget(self.l4)
         self.marginControls.addWidget(self.marginSize)
         
+        #Controls Layout
         self.controls = QtGui.QHBoxLayout()
         self.controls.addLayout(self.pixControls)
         self.controls.addSpacing(10)
@@ -83,27 +163,48 @@ class MainWindow(QtGui.QMainWindow):
         self.controls.addStretch()
         self.optionsGroup.setLayout(self.controls)
         
+        #Main Window Layout
         self.layout = QtGui.QVBoxLayout(self.w)
-        self.layout.addLayout(self.codeControls)
+        self.layout.addWidget(self.tabs)
         self.layout.addWidget(self.optionsGroup)
         self.layout.addWidget(self.qrcode, 1)
         self.layout.addLayout(self.buttons)
         self.layout.addWidget(self.exitButton)
 
-        QtCore.QObject.connect(self.lineEdit, QtCore.SIGNAL('textChanged(QString)'), self.qrencode)
-        QtCore.QObject.connect(self.saveButton, QtCore.SIGNAL('clicked()'), self.saveCode)
-        QtCore.QObject.connect(self.exitButton, QtCore.SIGNAL('clicked()'), self.close)
-        QtCore.QObject.connect(self.decodeFileAction, QtCore.SIGNAL('triggered()'), self.decodeFile)
-        QtCore.QObject.connect(self.decodeWebcamAction, QtCore.SIGNAL('triggered()'), self.decodeWebcam)
+        #Signals
+        self.textEdit.textChanged.connect(self.qrencode)
+        self.urlEdit.textChanged.connect(self.qrencode)
+        self.emailEdit.textChanged.connect(self.qrencode)
+        self.emailSubjectEdit.textChanged.connect(self.qrencode)
+        self.emailBodyEdit.textChanged.connect(self.qrencode)
+        self.smsNumberEdit.textChanged.connect(self.qrencode)
+        self.smsBodyEdit.textChanged.connect(self.qrencode)
+        self.telephoneEdit.textChanged.connect(self.qrencode)       
+        self.pixelSize.valueChanged.connect(self.qrencode)
+        self.ecLevel.currentIndexChanged.connect(self.qrencode)
+        self.marginSize.valueChanged.connect(self.qrencode)
+        self.saveButton.clicked.connect(self.saveCode)
+        self.exitButton.clicked.connect(self.close)
+        self.decodeFileAction.triggered.connect(self.decodeFile)
+        self.decodeWebcamAction.triggered.connect(self.decodeWebcam)
 
-    def qrencode(self, text):
+    def qrencode(self):
+        text = [
+            unicode(self.textEdit.toPlainText()),
+            unicode(self.urlEdit.text()),
+            ( unicode(self.emailEdit.text()), unicode(self.emailSubjectEdit.text()), unicode(self.emailBodyEdit.text()) ),
+            ( unicode(self.smsNumberEdit.text()), unicode(self.smsBodyEdit.text()) ),
+            unicode(self.telephoneEdit.text()),
+        ]
         level = (u'L',u'M',u'Q',u'H')
-        if text:
+        data_type = (u'text',u'url',u'email',u'sms',u'telephone')
+
+        if text[self.tabs.currentIndex()]:
             qr = QR(pixel_size = unicode(self.pixelSize.value()),
-                    data=text,
+                    data=text[self.tabs.currentIndex()],
                     level=unicode(level[self.ecLevel.currentIndex()]),
                     margin_size=unicode(self.marginSize.value()),
-                    data_type='text',
+                    data_type=unicode(data_type[self.tabs.currentIndex()]),
                     )
             if qr.encode() == 0:
                 self.qrcode.setPixmap(QtGui.QPixmap(qr.filename))
@@ -116,6 +217,8 @@ class MainWindow(QtGui.QMainWindow):
     def saveCode(self):
         fn = QtGui.QFileDialog.getSaveFileName(self, u'Save QRCode', filter=u'PNG Images (*.png);; All Files (*.*)')
         if fn:
+            if not fn.toLower().endsWith(u".png"):
+                fn += u".png"
             self.qrcode.pixmap().save(fn)
             print "Saving to file: %s" % fn
             QtGui.QMessageBox.information(self, u'Save QRCode',u'QRCode succesfully saved.')
